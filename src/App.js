c2be008af357820a42222e5aab39ebf446031103
IR~SAGE <sibo.egide98@gmail.com>
2021-02-07 14:28:34 +0200

    Creating The Header Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's Go</h1>
+<Header />
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import { ShoppingBasket } from "@material-ui/icons";
+import SearchIcon from "@material-ui/icons/Search";
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img alt=""
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+      <div className="header__search">
+              <input className="header__searchInput" type="text" />
+              <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Sage</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasket />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Header;
