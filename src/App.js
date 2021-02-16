de12cca69bdd126ffe86dfef770d298be8aa2647
IR~SAGE <sibo.egide98@gmail.com>
2021-02-16 15:30:03 +0200

    Designing the Payment Page And Connecting To Stripe

--- src/App.js
+++ src/App.js
@@ -7,43 +7,54 @@ import Checkout from "./components/Checkout";
 import Login from "./components/Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./components/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51IKPYcFLO2mX4C6ZZE7rMPZqSt66JnIraPZ8gvQWE2OZfGL3fIR3n6oUKBOZtdo6nYi1tsa4c5adbnIcAuSqPIFO00uRcX0IZs"
+);
 
 function App() {
-
-  const [{ }, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
-    auth.onAuthStateChanged(authUser => {
+    auth.onAuthStateChanged((authUser) => {
       if (authUser) {
-      //the user just looged in /the user was logged in
+        //the user just looged in /the user was logged in
         dispatch({
-          type: 'SET_USER',
-           user:authUser
-        })
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
         //the user is logged out
         dispatch({
-          type: 'SET_USER',
-           user:null
-        })
-    }
-  })
-},[])
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
 
   return (
     <Router>
       <div className="app">
-     
         <Switch>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/checkout">
-          <Header />
+            <Header />
             <Checkout />
           </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
           <Route path="/">
-          <Header />
+            <Header />
             <Home />
           </Route>
         </Switch>
